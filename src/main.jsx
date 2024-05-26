c37f7b4307d37c17a8c2628c8963f3c20561a500
ShubhamDaS-IIT-Dhanbad <shubhamdasiitcs@gmail.com>
2024-05-26 10:55:22 +0530

    Your commit message here

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App.jsx';
 import store from './redux/store/store.jsx';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </Provider>
-  </React.StrictMode>
-);
+
+try {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+} catch (error) {
+  console.error('Error rendering React application:', error);
+}
+
+
+

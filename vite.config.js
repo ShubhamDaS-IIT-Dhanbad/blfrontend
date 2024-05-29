60e20523370bfc97d9d70a544c5cf7a1736cd574
ShubhamDaS-IIT-Dhanbad <shubhamdasiitcs@gmail.com>
2024-05-29 22:31:47 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/url':'http://localhost:12000'
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:12000'
     }
   },
-  plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ensure the output directory is correctly specified
+    rollupOptions: {
+      // Ensure your entry point is specified correctly
+      input: '/src/main.jsx'
+    }
+  }
+});
